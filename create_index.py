f502bc1e7917843688f33ed646f0faaea83c7954
Maxime Portaz <maxime.portaz@gmail.com>
2019-12-16 14:55:01 +0100

    update

--- create_index.py
+++ create_index.py
@@ -3,11 +3,13 @@ import numpy as np
 import argparse
 import re
 from tqdm import trange
+import time
 
 
 def choose_train_size(index_key):
+
     # some training vectors for PQ and the PCA
-    n_train = 256 * 1000
+    n_train = 65000
 
     if "IVF" in index_key:
         matches = re.findall("IVF([0-9]+)", index_key)
@@ -17,12 +19,13 @@ def choose_train_size(index_key):
         matches = re.findall("IMI2x([0-9]+)", index_key)
         nbit = int(matches[0])
         n_train = max(n_train, 256 * (1 << nbit))
+    return 65000
     return n_train
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--nb_features", default=100000, type=int)
-parser.add_argument("--index_factory", default="IMI2x12,PQ16", type=str)
+parser.add_argument("--index_factory", default="OPQ8_32,IVF262144,PQ8", type=str)
 parser.add_argument("--n_train", default=-1, type=int)
 parser.add_argument("--output_dir", required=True, type=str)
 parser.add_argument("--index_version", required=True, type=str)
@@ -36,19 +39,17 @@ output_dir = args.output_dir
 index_file = index_factory.replace(",", "_") + args.index_version + ".index"
 
 
-if not index_factory.startswith("Flat"):
-
-    index = faiss.index_factory(
-        args.row_size, index_factory, faiss.METRIC_INNER_PRODUCT
-    )
-    n_train = choose_train_size(index_factory)
-    n_train = min(n_train, args.nb_features)
-    xt = np.random.random((n_train, args.row_size)).astype("float32").copy()
-    index.verbose = True
-    index.train(xt)
-    # faiss.write_index(index, output_dir+'/trained_'+index_file)
-else:
-    index = faiss.index_factory(ROW_SIZE, index_factory, faiss.METRIC_INNER_PRODUCT)
+# index = faiss.index_factory(args.row_size, index_factory, faiss.METRIC_INNER_PRODUCT)
+index = faiss.index_factory(args.row_size, index_factory)
+n_train = choose_train_size(index_factory)
+n_train = min(n_train, args.nb_features)
+xt = np.random.random((n_train, args.row_size)).astype("float32").copy()
+print("Using " + str(len(xt)) + " vectors to train")
+index.verbose = True
+t0 = time.time()
+index.train(xt)
+print("train done in %.3f s" % (time.time() - t0))
+# faiss.write_index(index, output_dir+'/trained_'+index_file)
 
 index_with_id = faiss.IndexIDMap(index)
 
